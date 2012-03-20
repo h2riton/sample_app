4dba4e06fb502b43b5e2e753db386786fcdfbef2
h2riton <hua.henri@gmail.com>
2012-03-20 22:34:13 +0100

    finish sign-in part

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -42,12 +42,12 @@ render_views
     end
     
     it "should be successful" do
-      get 'new'
+      get :new
       response.should be_success
     end
     
     it "should have the right title" do
-      get 'new'
+      get :new
       response.should have_selector("title", :content => "Sign up")
     end
     
